8a9e812185f08318c50bfe1869b14fc2b844a4f1
katlj <kathurnalj@gmail.com>
2023-12-17 14:24:33 -0500

    new changes and added concept comments

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
-export default function Square() {
-  return <button className="square">X</button>;
+export default function Square() {// our first component- is a piece of reusable code that shows on the screen
+ // export makes function accessible outisde of file, default means main function
+  return <button className="square">X</button>;// export
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+//this file is the bridge between the app components
+//and the web browser
+import React, { StrictMode } from "react"; // React
+import { createRoot } from "react-dom/client";// Reacts library to talk to web browser
+import "./styles.css";// styles for components
 
-import App from "./App";
+import App from "./App";// the component create din app.js
 
 const root = createRoot(document.getElementById("root"));
 root.render(
